7dc6ceb4778fcd4ad2894a2819bd2aa4ca2487a4
k14041kk <k14041kk@aitech.ac.jp>
2017-02-26 18:21:46 +0900

    追加：DB用のライブラリ

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -1,6 +1,6 @@
 var gulp    	 = require('gulp');
 
-var tasks = ['copy:html','copy:material','copy:css','compile:ts'];
+var tasks = ['copy:html','copy:material','copy:css','copy:dexie','compile:ts'];
 
 gulp.task('build',tasks,function(callback) {
 
